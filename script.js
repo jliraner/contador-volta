83ff96c0559ec5cdcfde46ef4d3482296a954913
jliraner <143611278+jliraner@users.noreply.github.com>
2023-08-30 14:29:07 +0200

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const daysX = document.getElementById('days');
-const hoursX = document.getElementById('horas');
-const minutesX = document.getElementById('minutos');
-const secondsX = document.getElementById('segundos');
+const hoursX = document.getElementById('hours');
+const minutesX = document.getElementById('minutes');
+const secondsX = document.getElementById('seconds');
 
-const bdate = "20 Jan 2024"
+const bdate = "16 Sep 2023"
 
 function contador(){
 
@@ -15,25 +15,24 @@ function contador(){
     const tempo = (DataAniversario - DataAtual)/1000;
 
     const days = Math.floor(tempo/3600/24);
-    const horas = Math.floor(tempo/3600) % 24;
-    const minutos = Math.floor(tempo/60) % 60;
-    const segundos = Math.floor(tempo) % 60;
+    const hours = Math.floor(tempo/3600) % 24;
+    const minutes = Math.floor(tempo/60) % 60;
+    const seconds = Math.floor(tempo) % 60;
 
     daysX.innerHTML = days;
-    hoursX.innerHTML = horas;
-    minutesX.innerHTML = minutos;
-    secondsX.innerHTML = segundos;
+    hoursX.innerHTML = hours;
+    minutesX.innerHTML = minutes;
+    secondsX.innerHTML = seconds;
 
 
     console.log(days)
-
-
 }
 
 
 contador();
 
 setInterval(contador, 1000);
+
 
 
 
